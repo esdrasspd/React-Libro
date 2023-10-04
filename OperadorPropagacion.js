298e1f52c10d4e274f3d46175b7f870d4bb0c5df
Abel Diaz <abeldiaz2307@gmail.com>
2023-10-03 21:51:02 -0600

    Leccion de operador propagacion

---
+++ OperadorPropagacion.js
@@ -0,0 +1,6 @@
+const montañas = ["Tallac", "Ralston", "Rose"];
+const cañones = ["Ward", "Blackwood"];
+const todo = [...montañas, ...cañones];
+
+console.log(todo); // ["Tallac", "Ralston", "Rose", "Ward", "Blackwood"]
+
