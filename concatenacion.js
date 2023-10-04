62087b0dbeca1036fc17ba135edf14d6fbaacf0b
Abel Diaz <abeldiaz2307@gmail.com>
2023-10-03 21:49:02 -0600

    Leccion de concatenacion

---
+++ concatenacion.js
@@ -0,0 +1,9 @@
+var nombre = "Esdras";
+var segundoNombre = "Abel";
+var apellido = "Sapón";
+
+console.log(apellido + "," + nombre + " " + segundoNombre);
+
+//Otra forma
+
+console.log('${apellido}, ${nombre} ${segundoNombre}');
